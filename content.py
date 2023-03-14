05a18518d63b728cc911c197f8d28b862bfbc75b
MineBelgaid <amin-belgaid@hotmail.com>
2023-03-14 19:55:16 +0000

    add code in files types: .py remove code in files types:

---
+++ content.py
@@ -0,0 +1,12 @@
+import git
+from git_contributions_importer import *
+
+
+repo = git.Repo("P:/JIraBitBucket/ecos-scrape")
+mock_repo = git.Repo("P:/FreeLance/mock-repo")
+
+importer = Importer([repo], mock_repo)
+importer.set_author("amine.a.belgaid@gmail.com")
+
+importer.import_repository()
+print("labox")
